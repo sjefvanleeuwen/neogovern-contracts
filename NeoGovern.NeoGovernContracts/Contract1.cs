7f0b5a03bd055773c6a1b76032bb31be1baa6a59
Sjef van Leeuwen <36629650+sjefvanleeuwen@users.noreply.github.com>
2018-04-12 15:52:55 +0000

    Add project files.

---
+++ NeoGovern.NeoGovernContracts/Contract1.cs
@@ -0,0 +1,13 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace NeoGovern.NeoGovernContracts
+{
+    public class Contract1 : SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+<p align=center>
+    <img src="./doc/img/neogovern.png" />
+</p>
+<br/>
+
+
+### SMART Contracts
+
+This project contains the smart contracts for Sovereign identities using the DID specification.
+
+### Status
+
+Currently this is a research project and is not meant for production.
