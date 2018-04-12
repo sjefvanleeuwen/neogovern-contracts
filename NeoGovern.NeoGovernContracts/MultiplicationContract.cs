391b7f025fa53a13023617db8c7777f545e94b50
Sjef van Leeuwen <36629650+sjefvanleeuwen@users.noreply.github.com>
2018-04-12 18:29:49 +0000

    MultiplicationContract

---
+++ NeoGovern.NeoGovernContracts/MultiplicationContract.cs
@@ -0,0 +1,20 @@
+﻿using Neo.SmartContract.Framework;
+
+namespace NeoGovern.NeoGovernContracts
+{
+    public class Multiplication : SmartContract
+    {
+        public static int Main(int a, int b, int c)
+        {
+            if (a > b)
+                return a * Multiply(b, c);
+            else
+                return Multiply(a, b) * c;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ NeoGovern.NeonGovernContractsTests/ContractTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.VM;
+
+namespace NeoGovern.NeonGovernContractsTests
+{
+    [TestClass]
+    public class ContractTests
+    {
+        [TestMethod]
+        public void ShouldReturn24WhenMultiplationOf3Times4Times2()
+        {
+            const string abi = "압汫歶刀쑺癬八穒泄歶剒쑺污歶쌀癬八ꃃ癬卫穒泄歶썓≤氀歶쌀癬八泃歶썒籡㙥销癬呫穒拄\"癬k泃歶썑籡ᱥ氀歶썒沕歶剔쑺͢氀歶썔污晵앓汫歶刀쑺癬八穒懄癬k泃歶썑沕歶剒쑺͢氀歶썒污晵";
+
+            var engine = new ExecutionEngine(null, null);
+            engine.LoadScript(Encoding.Unicode.GetBytes(abi));
+
+            using (var sb = new ScriptBuilder())
+            {
+                int[] parameter = { 3, 4, 2 };
+                parameter.Reverse().ToList().ForEach(p => sb.EmitPush(p));
+                engine.LoadScript(sb.ToArray());
+            }
+            // Execute 3 * 4 * 2
+            engine.Execute();
+            var result = engine.EvaluationStack.Peek().GetBigInteger();
+            Assert.IsTrue(result == 24);
+        }
+    }
+}
